2fcd7b077360aa12864068a1f1ed9b46297daf1e
Luciano Augusto Campagnoli da Silva <luti4497@gmail.com>
2022-05-22 16:29:34 -0300

    fix: Changes arrow functions to normal JS function calls

--- assembly/index.ts
+++ assembly/index.ts
@@ -2,10 +2,10 @@ import { PersistentUnorderedMap } from "near-sdk-as";
 
 export const products = new PersistentUnorderedMap<string, string>("PRODCUTS");
 
-export const setProduct = (id: string, productName: string): void => {
+export function setProduct(id: string, productName: string): void {
     products.set(id, productName);
 }
 
-export const getProduct = (id: string): string | null => {
+export function getProduct(id: string): string | null {
     return products.get(id);
 }
